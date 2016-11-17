d0b3a3c87f8997e3b944e9d1c4b3d6237142a86d
AngheloAlf <anghelo.carvajal.14@sansano.usm.cl>
2016-11-17 12:13:26 -0300

    Comenzando y modularizando el template

--- www/index.php
+++ www/index.php
@@ -5,4 +5,34 @@
  * Time: 19:30
  */
 
+
+?>
+
+<html>
+    <head>
+        <?php include("includes/head.phtml"); ?>
+    </head>
+    <body>
+        <div id="conteneur">
+            <?php include("includes/header.phtml"); ?>
+            <div id="contenu">
+                <div id="left">
+                    <?php include("includes/nav.phtml"); ?>
+                </div>
+                <div id="right"><!-- COLONNE DE DROITE -->
+                    <?php include("url/home.phtml"); ?>
+                </div> <!-- FIN CONTENU -->
+
+            </div>
+            <div id="pied">
+                <?php include("includes/footer.phtml"); ?>
+            </div>
+        </div>
+    </body>
+
+</html>
+
+<?php
+
+
 ?>
